7fabf0774300ca8bf1c9b598a09ace75ef35e7ff
hicass <cassidyjwalters@gmail.com>
2023-06-23 15:48:05 -0700

    Add Note model, and functionality for a 'No Notes Yet' msg to be displayed if there are no notes

---
+++ src/components/NoteBox/NoteBox.jsx
@@ -0,0 +1,7 @@
+export default function NoteBox() {
+    return (
+        <>
+        <p>hello</p>
+        </>
+    )
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,26 +1,32 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
+import NoteBox from '../../components/NoteBox/NoteBox';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const [notes, setNotes] = useState([
+    {},
+    {},
+  ]);
 
   return (
     <main className="App">
       { user ?
-          <>
+          <section>
             <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
-          </>
+            {notes.length ?
+              <div>
+                {notes.map((note, idx) => (
+                  <NoteBox />
+                ))}
+              </div>
+            :
+              <p>No Notes Yet!</p>
+            }
+          </section>
           :
           <AuthPage setUser={setUser} />
       }
